7de3cabce537d606e73b61c277068d44996ee04c
2s4u <93463647+2s4u@users.noreply.github.com>
2023-01-25 22:08:27 -0500

    added shaders + organization

--- Sivine/Main.cpp
+++ Sivine/Main.cpp
@@ -11,13 +11,13 @@
 
 // Vertices coordinates
 GLfloat vertices[] =
-{
-	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
-	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
-	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
-	-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
-	0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner right
-	0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Inner down
+{ //                COORDINATES                      /      COLORS       //
+	-0.5f,  -0.5f * float(sqrt(3)) / 3,      0.0f,   0.8f, 0.3f,  0.02f, // Lower left corner
+	 0.5f,  -0.5f * float(sqrt(3)) / 3,      0.0f,   0.8f, 0.3f,  0.02f, // Lower right corner
+	 0.0f,   0.5f * float(sqrt(3)) * 2 / 3,  0.0f,   1.0f, 0.6f,  0.32f, // Upper corner
+	-0.25f,  0.5f * float(sqrt(3)) / 6,      0.0f,   0.9f, 0.45f, 0.17f, // Inner left
+	 0.25f,  0.5f * float(sqrt(3)) / 6,      0.0f,   0.9f, 0.45f, 0.17f, // Inner right
+	 0.0f,  -0.5f * float(sqrt(3)) / 3,      0.0f,   0.8f, 0.3f, 0.02f,  // Inner down
 };
 
 // Indices for vertices order
@@ -78,7 +78,8 @@ int main()
 	EBO EBO1(indices, sizeof(indices));
 
 	// Links VBO to VAO
-	VAO1.LinkVBO(VBO1, 0);
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 	// Unbind all to prevent accidentally modifying them
 	VAO1.Unbind();
 	VBO1.Unbind();
--- Sivine/VAO.h
+++ Sivine/VAO.h
@@ -10,7 +10,7 @@ public:
 	GLuint ID;
 	VAO();
 
-	void LinkVBO(VBO VBO, GLuint layout);
+	void LinkAttrib(VBO& VBO, GLuint layout, GLuint numComponents, GLenum type, GLsizeiptr stride, void* offset);
 	void Bind();
 	void Unbind();
 	void Delete();
